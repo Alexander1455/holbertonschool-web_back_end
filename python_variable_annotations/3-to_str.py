3d9ebee2a3e09c930a3e9c1794e8fafdb8f183d5
Alexander1455 <alexander21111121@gmail.com>
2023-09-28 11:31:52 -0500

    Convert a float to its string representation

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" 
+Convert a float to its string representation.
+
+Args:
+    n (float): The input floating-point number.
+
+Returns:
+str: The string representation of the input number.
+
+Example:
+    >>> to_str(5.7)
+     '5.7'
+    >>> to_str(9.2)
+    '9.2'
+"""
+
+
+def to_str(n: float) -> str:
+    return str(n)
