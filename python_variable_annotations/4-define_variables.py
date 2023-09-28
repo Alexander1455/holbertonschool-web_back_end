f02dfe0d22cf0f049a93dc912da6ed4ea2a55225
Alexander1455 <alexander21111121@gmail.com>
2023-09-28 11:40:05 -0500

    variable

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+a: variable int.
+
+pi: variable float.
+
+i_understand_annotations: variable boleana.
+
+school: variable string.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
