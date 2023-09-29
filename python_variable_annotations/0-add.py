e2733e075211c346efa261dfe31dddef0fe4945f
Alexander1455 <alexander21111121@gmail.com>
2023-09-28 19:14:52 -0500

    documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """
-def add(): sum of two floats 
+def add(): sum of two floats
 """
 
 
 def add(a: float, b: float) -> float:
+    """ type-annotated function add"""
     return a + b
