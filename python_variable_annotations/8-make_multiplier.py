5020a85480050487dcad4aca4fa6f9c3acee9bf1
Alexander1455 <alexander21111121@gmail.com>
2023-09-28 20:17:09 -0500

    correcting documentation

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -13,6 +13,7 @@ Returns:
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Create and return a multiplier function"""
     def multiplier_function(x: float) -> float:
+        """The multiplier value"""
         return x * multiplier
 
     return multiplier_function
