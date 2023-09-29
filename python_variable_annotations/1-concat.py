3631024652bbe0e7349643828c93da29d4471dab
Alexander1455 <alexander21111121@gmail.com>
2023-09-28 20:23:13 -0500

    correcting documentation

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""concatenate two strings """
+""" concatenate two strings """
 
 def concat(str1: str, str2: str) -> str:
-    """concatenate two strings """
+    """ function concat """
     return str1 + str2
