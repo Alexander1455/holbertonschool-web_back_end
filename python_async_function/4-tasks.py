303fdca7f5faf7701e58f7740dfb6054183c16d2
Alexander1455 <alexander21111121@gmail.com>
2023-10-01 19:00:20 -0500

    new code

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-""" A simple asynchronous coroutine. """
+""" Let's execute multiple coroutines at the same time with async """
 import asyncio
 from typing import List
+import random
 
-task_wait_random = __import__('3-tasks').task_wait_random
-
+task_wait_random = __import__('0-basic_async_syntax').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ Run multiple coroutines at the same time with async """
-    var = [task_wait_n(max_delay) for i in range(n)]
-    finish = [await task for task in asyncio.as_completed(var)]
-    return finish
+    """ Function that returns the list """
+    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
+    completed_tasks = await asyncio.gather(*tasks)
+    return completed_tasks
