6c70361b1ebbca9b7426a27344b1f6905d13e965
Alexander1455 <alexander21111121@gmail.com>
2023-10-28 23:52:06 -0500

    returns a Promise

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,13 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+  // Realiza alguna operación asincrónica aquí
+  // Por ejemplo, puedes usar fetch() para hacer una solicitud a una API
+
+  // Simulando una Promesa resuelta con un temporizador
+  setTimeout(() => {
+  resolve("Datos de respuesta de la API"); // Reemplaza esto con la respuesta real de la API
+  }, 1000); // Simulando un retraso de 1 segundo
+  });
+}
+
+export default getResponseFromAPI;
