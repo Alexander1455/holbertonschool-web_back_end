4c39af1fa3ef219159499c222b7905cf2ce947a4
Alexander1455 <alexander21111121@gmail.com>
2023-10-29 00:30:36 -0500

    Passes lint

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,12 +1,12 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-  // Realiza alguna operación asincrónica aquí
-  // Por ejemplo, puedes usar fetch() para hacer una solicitud a una API
+  return new Promise((resolve) => {
+      // Realiza alguna operación asincrónica aquí
+      // Por ejemplo, puedes usar fetch() para hacer una solicitud a una API
 
-  // Simulando una Promesa resuelta con un temporizador
-  setTimeout(() => {
-  resolve("Datos de respuesta de la API"); // Reemplaza esto con la respuesta real de la API
-  }, 1000); // Simulando un retraso de 1 segundo
+      // Simulando una Promesa resuelta con un temporizador
+      setTimeout(() => {
+          resolve('Datos de respuesta de la API'); // Reemplaza esto con la respuesta real de la API
+      }, 1000); // Simulando un retraso de 1 segundo
   });
 }
 
