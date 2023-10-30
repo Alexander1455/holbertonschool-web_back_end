c7b2c1c6e871d44e22e059a73d48c110b5273d0e
Alexander1455 <alexander21111121@gmail.com>
2023-10-29 23:06:48 -0500

    ArrayBuffer with an Int8 value at a position

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,19 @@
+function createInt8TypedArray(length, position, value) {
+  // Verificamos si la posición está dentro del rango válido.
+  if (position < 0 || position >= length) {
+    throw new Error("Position outside range");
+  }
+
+  // Creamos un ArrayBuffer de la longitud especificada.
+  const buffer = new ArrayBuffer(length);
+
+  // Creamos una vista DataView del ArrayBuffer.
+  const view = new DataView(buffer);
+
+  // Establecemos el valor Int8 en la posición especificada.
+  view.setInt8(position, value);
+
+  return view;
+}
+
+export default createInt8TypedArray;
