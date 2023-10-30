a0e52d800269e14b85b21f2f1aa1cadb44f5835b
Alexander1455 <alexander21111121@gmail.com>
2023-10-29 23:10:26 -0500

    returns a map for items with quantity set to 1

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+function updateUniqueItems(map) {
+  // Verificamos si el argumento es un mapa.
+  if (!(map instanceof Map)) {
+    throw new Error("Cannot process");
+  }
+
+  // Iteramos a través de las entradas del mapa.
+  for (const [key, value] of map.entries()) {
+    if (value === 1) {
+      // Actualizamos la cantidad a 100 si es igual a 1.
+      map.set(key, 100);
+    }
+  }
+}
+
+export default updateUniqueItems;
