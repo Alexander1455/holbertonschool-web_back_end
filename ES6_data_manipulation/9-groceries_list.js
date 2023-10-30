2bb431f896da1c2c7bf7a70021239b33a7202e95
Alexander1455 <alexander21111121@gmail.com>
2023-10-29 23:09:41 -0500

    returns a grocery map of the items

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+function groceriesList() {
+  // Creamos un nuevo objeto Map y agregamos los elementos y cantidades como pares clave-valor.
+  const groceryMap = new Map([
+      ['Apples', 10],
+      ['Tomatoes', 10],
+      ['Pasta', 1],
+      ['Rice', 1],
+      ['Banana', 5],
+  ]);
+
+  return groceryMap;
+}
+
+export default groceriesList;
