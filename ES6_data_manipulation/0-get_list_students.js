fecc9b1bb858c1d50dbaf2453c75407e77a61ab8
Alexander1455 <alexander21111121@gmail.com>
2023-10-29 23:03:05 -0500

    simple list

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+function getListStudents() {
+  // Creamos y retornamos un array de objetos que representan a los estudiantes.
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
+
+export default getListStudents;
