7fb13ccd04a4ee6a33581515a00dbf65f68e69e7
Alexander1455 <alexander21111121@gmail.com>
2023-10-29 23:08:01 -0500

    returns a boolean value if all elements exist

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,6 @@
+function hasValuesFromArray(set, arr) {
+  // Utilizamos el método every para verificar si todos los elementos del array están presentes en el set.
+  return arr.every((element) => set.has(element));
+}
+
+export default hasValuesFromArray;
